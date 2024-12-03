acea3a8b8a59826b12410b2570700bb149653fe9
Foxrim <Flavien085@gmail.com>
2024-12-03 14:43:35 +0100

    [feat] - cards catégories

---
+++ client/src/components/CardCategories.tsx
@@ -0,0 +1,14 @@
+import "./CardCategories.css";
+
+export default function Card() {
+  return (
+    <div className="cardContainer">
+      <figure>
+        <img src="#" alt="#" />
+      </figure>
+      <div>
+        <h3>TEXT TITRE</h3>
+      </div>
+    </div>
+  );
+}
